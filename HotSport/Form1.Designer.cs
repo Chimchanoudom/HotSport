31c18bb6fe91d9d83d15043e525971dc277e1379
Chimchanouodm <chanoudomchim@gmail.com>
2018-08-08 14:41:27 +0700

    Initial commit

---
+++ HotSport/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace HotSport
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSSID = new System.Windows.Forms.TextBox();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSSID
+            // 
+            this.txtSSID.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSSID.Location = new System.Drawing.Point(177, 35);
+            this.txtSSID.Name = "txtSSID";
+            this.txtSSID.Size = new System.Drawing.Size(215, 26);
+            this.txtSSID.TabIndex = 0;
+            this.txtSSID.Text = "gg";
+            // 
+            // txtKey
+            // 
+            this.txtKey.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKey.Location = new System.Drawing.Point(177, 67);
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(215, 26);
+            this.txtKey.TabIndex = 0;
+            this.txtKey.Text = "12345678";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "HotSpoted Name :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(30, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password :";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCreate.Location = new System.Drawing.Point(62, 127);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 33);
+            this.btnCreate.TabIndex = 2;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(188, 127);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 33);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(312, 127);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 33);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 193);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtKey);
+            this.Controls.Add(this.txtSSID);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSSID;
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ HotSport/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace HotSport
+{
+    public partial class Form1 : Form
+    {
+        private ProcessStartInfo go = null;
+        private string message;
+        Process p = null;
+        public Form1()
+        {
+            InitializeComponent();
+            first();
+            
+           
+        }
+        public void first()
+        {
+            
+            go = new ProcessStartInfo("cmd.exe");
+            go.UseShellExecute = false;
+            go.RedirectStandardOutput = true;
+            go.CreateNoWindow = true;
+            go.FileName = "netsh";
+        }
+        public void start()
+        {
+           
+            go.Arguments = "wlan start hosted network";
+            p = Process.Start(go);
+            message = p.StandardOutput.ReadToEnd()+"\n";
+            MessageBox.Show(message);
+        }
+        public void stop()
+        {
+            go.Arguments = "wlan stop hosted network";
+            p = Process.Start(go);
+            message = p.StandardOutput.ReadToEnd() + "\n";
+            MessageBox.Show(message);
+        }
+        public void create(string ssid,string key)
+        {
+            go.Arguments = string.Format("wlan set hostednetwork mode=allow ssid={0} key={1}",ssid,key);
+            executt(go);
+        }
+        public void executt(ProcessStartInfo go)
+        {
+            bool execut = false;
+            try
+            {
+                using (Process p =  Process.Start(go))
+                {
+                    message = p.StandardOutput.ReadToEnd() + "\n";
+                    p.WaitForExit();
+                    execut = true;
+                    MessageBox.Show(message);
+                    start();
+                }
+            }
+            catch (Exception e)
+            {
+                message = "";
+                message += e.Message;
+                execut = false;
+            }
+        }
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            
+            create(txtSSID.Text, txtKey.Text);
+           
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+
+            stop();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            start();
+        }
+    }
+}
